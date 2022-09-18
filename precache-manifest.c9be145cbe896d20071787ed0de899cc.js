bbc844f1e6a1b1285b9898b51ecaac419d37a717
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-09-18 15:06:29 +0530

    Updates

---
+++ precache-manifest.c9be145cbe896d20071787ed0de899cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7ed6dedffc879be0602",
+    "url": "/My_portfolio/static/css/main.8a3d175d.chunk.css"
+  },
+  {
+    "revision": "b7ed6dedffc879be0602",
+    "url": "/My_portfolio/static/js/main.b7ed6ded.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/My_portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c7504fb16af3a1cc0929",
+    "url": "/My_portfolio/static/js/runtime~main.c7504fb1.js"
+  },
+  {
+    "revision": "d7d917239f9331e0ee7c9b615abf4bfb",
+    "url": "/My_portfolio/index.html"
+  }
+];
