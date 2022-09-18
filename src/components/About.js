d427b78380e110b601bc0a9ce8d8c8b9fc436296
vineeth-kumars <sattinenivineethkumar@gmail.com>
2022-09-18 15:01:04 +0530

    committing the portfolio

---
+++ src/components/About.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="item-img"  src="images/myImage.png" alt="" />
+
+            </div>
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 resumeData.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{resumeData.name}</span>
+                     <br></br>
+       						   <span>
+                     {resumeData.address}
+                    </span>
+                    <br></br>
+                    <span>sattinenivineethkumar@gmail.com</span>
+                    <br></br>
+                    <span>9908216181</span>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+  imagebaseurl: "images/myImage.png",
+  name: "Vineeth Kumar",
+  role: "Frontend Developer ",
+  linkedinId: "SAI VINEETH KUMAR SATTINENI",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " Good working experience in developing websites and web applications. Worked on Single Page web application development using ReactJS and Spring Boot. Worked in Agile teams and scrum to deliver great capability web Apps to the end-users.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url:
+        "https://www.linkedin.com/in/sai-vineeth-kumar-sattineni-082793197?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B99u0IykaRv6%2FSw072WmWMw%3D%3D",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/vineeth-kumars",
+      className: "fa fa-github",
+    },
+    // {
+    //   "name":"skype",
+    //   "url":"http://twitter.com/rbhatia46",
+    //   "className":"fa fa-twitter"
+    // }
+  ],
+  aboutme:
+    "I am currently a software engineer at Arshaa Technologies Pvt. Ltd. . I am a self taught Full Stack Web Developer, currently diving deeper into Web Development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://rbhatia46.github.io",
+  education: [
+    {
+      UniversityName: "Pragati Engineering College",
+      specialization: "Information Technology",
+      MonthOfPassing: "sep",
+      YearOfPassing: "2019",
+      Achievements: "",
+    },
+    {
+      UniversityName: "Narayan Junior College",
+      specialization: "MPC",
+      MonthOfPassing: "Mar",
+      YearOfPassing: "2015",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Arshaa Technologies Pvt. Ltd.",
+      specialization: "Software Engineer",
+      MonthOfLeaving: "Nov",
+      YearOfLeaving: "2021",
+      Achievements: "Cerificate of Appriciation",
+    },
+    // {
+    //   CompanyName: "Some Company",
+    //   specialization: "Some specialization",
+    //   MonthOfLeaving: "Jan",
+    //   YearOfLeaving: "2018",
+    //   Achievements: "Some Achievements",
+    // },
+  ],
+  skillsDescription: "My skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "OTP sending ",
+      description: "mobileapp",
+      imgurl: "images/OtpSC.png",
+      url:"https://vineeth-kumars.github.io/Kisan_project/",
+    },
+    // {
+    //   name: "project2",
+    //   description: "mobileapp",
+    //   imgurl: "images/portfolio/project.jpg",
+    // },
+    // {
+    //   name: "project3",
+    //   description: "mobileapp",
+    //   imgurl: "images/portfolio/project2.png",
+    // },
+    // {
+    //   name: "project4",
+    //   description: "mobileapp",
+    //   imgurl: "images/portfolio/phone.jpg",
+    // },
+  ],
+  testimonials: [
+    {
+      description: "The calm and composed attitude ofVineeth is much appreciated across the firm. He is always available to help us with crucial changes and deployements. Though he was assigned only the UI tasks , he himself learnt and implemented backend services and deployments . Your continous support to the PE client added a lot of value",
+      name: "Arshaa Technologies Mangement Team",
+    },
+    {
+      description: "I worked with Vineeth kumar for 3 months for a personal project .He has sound knowledge of frontend technologies like javascript and ReactJS. He is Vesrsatile , good team player as well as leads",
+      name: "Ravi Teja , perficient",
+    },
+  ],
+};
+
+export default resumeData;
